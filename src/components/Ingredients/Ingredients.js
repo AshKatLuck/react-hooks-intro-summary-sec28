1afe605abe5d4b13801e074326ed5688b0a77aaf
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-30 17:32:26 -0400

    add a firebase db ingredient.json and add the ingredient to it in the addHandler

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,25 @@ function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
   const onAddIngredientHandler = (ingredient) => {
-    setIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      "https://react-hooks-demo-91469-default-rtdb.firebaseio.com/ingredient.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const onRemoveIngredient = (id) => {
