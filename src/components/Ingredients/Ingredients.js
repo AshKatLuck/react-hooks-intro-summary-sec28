ac87f23d9f7d433352f1372b6751c73d1184a4ac
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-04-01 21:28:12 -0400

    set isLoading and error states

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,9 +3,12 @@ import React, { useState, useEffect, useCallback } from "react";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   // useEffect(() => {
   //   console.log("rendering");
@@ -30,48 +33,66 @@ function Ingredients() {
   }, []);
 
   const onAddIngredientHandler = (ingredient) => {
-    fetch(
-      "https://react-hooks-demo-91469-default-rtdb.firebaseio.com/ingredient.json",
-      {
-        method: "POST",
-        body: JSON.stringify(ingredient),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        setIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
-      });
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch(
+        "https://react-hooks-demo-91469-default-rtdb.firebaseio.com/ingredient.json",
+        {
+          method: "POST",
+          body: JSON.stringify(ingredient),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseData) => {
+          setIngredients((prevIngredients) => [
+            ...prevIngredients,
+            { id: responseData.name, ...ingredient },
+          ]);
+        });
+      setIsLoading(false);
+    }, 1500);
   };
 
   const onRemoveIngredient = (id) => {
-    console.log(id);
+    setIsLoading(true);
     fetch(
       `https://react-hooks-demo-91469-default-rtdb.firebaseio.com/ingredient/${id}.json`,
       {
         method: "DELETE",
       }
-    ).then((response) => {
-      console.log(response);
-      setIngredients((prevIngredients) =>
-        prevIngredients.filter((ingredient) => ingredient.id !== id)
-      );
-    });
+    )
+      .then((response) => {
+        console.log(response);
+        setIngredients((prevIngredients) =>
+          prevIngredients.filter((ingredient) => ingredient.id !== id)
+        );
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+    setIsLoading(false);
   };
 
   const onLoadFilteredIngredients = useCallback((filteredIngredients) => {
     setIngredients(filteredIngredients);
   }, []);
+
+  const clearErrorModal = () => {
+    setError(null);
+  };
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={onAddIngredientHandler} />
+      {error && <ErrorModal onClose={clearErrorModal}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={onAddIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={onLoadFilteredIngredients} />
