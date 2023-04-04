a22a51adc9b6546562acddfa56cca9220c10fa85
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-04-03 23:02:44 -0400

    use custom hook in the search.js'

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
+import useHttp from "../../hooks/http";
+import ErrorModal from "../UI/ErrorModal";
 
 const Search = React.memo((props) => {
   const [enteredFilter, setEnteredFilter] = useState("");
+  const { isLoading, error, clear, data, sendRequest } = useHttp();
   const { onLoadIngredients } = props;
   const inputRef = useRef();
   useEffect(() => {
@@ -14,35 +17,36 @@ const Search = React.memo((props) => {
           enteredFilter.length === 0
             ? ""
             : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch(
+        sendRequest(
           "https://react-hooks-demo-91469-default-rtdb.firebaseio.com/ingredient.json" +
-            query
-        )
-          .then((response) => response.json())
-          .then((responseData) => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount,
-              });
-              //logic
-
-              onLoadIngredients(loadedIngredients);
-            }
-          });
+            query,
+          "GET"
+        );
       }
     }, 500);
     return () => {
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+  }, [enteredFilter, inputRef, sendRequest]);
+  useEffect(() => {
+    const loadedIngredients = [];
+    for (const key in data) {
+      loadedIngredients.push({
+        id: key,
+        title: data[key].title,
+        amount: data[key].amount,
+      });
+    }
+    //logic
+    onLoadIngredients(loadedIngredients);
+  }, [data, onLoadIngredients]);
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             type="text"
             value={enteredFilter}
